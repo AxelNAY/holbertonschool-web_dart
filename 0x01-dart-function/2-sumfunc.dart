c1eb9c7f138f992976b96bacf6bebc6a2ea54831
AxelNAY <axelnay@sfr.fr>
2025-12-12 15:46:15 +0100

    Update 2-sumfunc.dart

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -9,6 +9,6 @@ int sub(int a, int b) {
 String showFunc(int a, int b) {
   int sum = add(a, b);
   int substract = sub(a, b);
-  String str = "Add $a + $b = $sum\nSub $a + $b = $substract";
+  String str = "Add $a + $b = $sum\nSub $a - $b = $substract";
   return str;
 }
