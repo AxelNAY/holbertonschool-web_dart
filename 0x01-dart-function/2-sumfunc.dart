5a77ea2e4b800accdd890a4f17cf777bb75c1643
AxelNAY <axelnay@sfr.fr>
2025-12-12 15:44:27 +0100

    Complete files 2-

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,14 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  int sum = add(a, b);
+  int substract = sub(a, b);
+  String str = "Add $a + $b = $sum\nSub $a + $b = $substract";
+  return str;
+}
