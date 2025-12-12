be86580fda5326ba5d71f9710a7713bb6e18bbd9
AxelNAY <axelnay@sfr.fr>
2025-12-12 16:30:09 +0100

    Complete files 10-

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,24 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+  
+  String reversed = s.split('').reversed.join('');
+  return s == reversed;
+}
+
+String longestPalindrome(String s) {
+  String longest = "";
+  
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 3; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
+    }
+  }
+  
+  return longest.isEmpty ? "none" : longest;
+}
